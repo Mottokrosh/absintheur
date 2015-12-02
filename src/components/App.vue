d08093e898fca27a38e7d392addcd7b85a73af68
Frank Reding <frank.reding@gmail.com>
2015-12-02 11:30:11 +0000

    Component scaffolding

---
+++ src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<nav>
+		<div>
+			<ul>
+				<li><a v-link="'home'">Home</a></li>
+				<li><a v-link="'login'">Login</a></li>
+				<li><a v-link="'signup'">Sign Up</a></li>
+				<li><a v-link="'secretquote'">Secret Quote</a></li>
+				<li><a v-link="'login'">Logout</a></li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container">
+		<router-view></router-view>
+	</div>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<h1>Get a Free Chuck Norris Quote!</h1>
+		<button v-on:click="getQuote()">Get a Quote</button>
+		<div v-if="quote">
+			<h2><blockquote>{{ quote }}</blockquote></h2>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			quote: ''
+		}
+	},
+	methods: {
+		getQuote() {
+			this.$http
+				.get('http://localhost:3001/api/random-quote', (data) => {
+					this.quote = data;
+				})
+				.error((err) => console.log(err))
+		}
+	}
+}
+</script>
