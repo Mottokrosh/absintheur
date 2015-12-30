b26379551ed75922c1da143cea0ea5e9ad63c3b1
Frank Reding <frank.reding@gmail.com>
2015-12-30 15:22:12 +0000

    Page transitions

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,13 @@
 <template>
-	<nav role="navigation">
-		<ul>
-			<li><a v-link="'home'">Home</a></li>
-			<li><a v-link="'library'">Library</a></li>
-			<li><a v-link="'shop'">Shop</a></li>
-			<li><a v-link="'collection'">Collection</a></li>
-		</ul>
-	</nav>
 	<div class="container">
+		<nav role="navigation">
+			<ul>
+				<li><a v-link="'home'">Home</a></li>
+				<li><a v-link="'library'">Library</a></li>
+				<li><a v-link="'shop'">Shop</a></li>
+				<li><a v-link="'collection'">Collection</a></li>
+			</ul>
+		</nav>
 		<router-view transition="slide"></router-view>
 	</div>
 </template>
